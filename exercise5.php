e5f3ee01ab2aec721862e8ad0a734a70d99653c1
krishnakumarkp <krishnakumarkp@gmail.com>
2012-10-10 16:55:12 -0700

    simple exercises

---
+++ exercise5.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+    </head>
+    <body>
+    <?php
+       $name = 'krishna';
+       var_dump($name);
+       echo "<br>";
+       print_r($name);
+       echo "<br>";
+       $age =35;
+       var_dump($age);
+       echo "<br>";
+       print_r($age);
+
+    ?>
+    </body>
+</html>
